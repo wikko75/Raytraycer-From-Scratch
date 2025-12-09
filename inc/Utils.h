dd8f59d9a961cad584bfdb9fa8bc9ce6cc8a44cc
Wiktor <wiktorkar25@gmail.com>
2025-12-09 16:27:07 +0100

    Add random real numbers generation utility functions

--- inc/Utils.h
+++ inc/Utils.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <limits>
 #include <numbers>
-
+#include <random>
 
 namespace constants {
 
@@ -19,5 +19,17 @@ namespace utility_functions {
 	constexpr auto degrees(double radians) -> double
 	{
 		return (radians / constants::pi) * 180;
+	}
+
+	[[nodiscard]] inline auto random_real_number() -> float
+	{
+		thread_local std::mt19937 generator{ std::random_device{}() };
+		thread_local std::uniform_real_distribution<float> dist{ 0.0f, 1.0f };
+		return dist(generator);
+	}
+
+	[[nodiscard]] inline auto random_real_number(float range_min, float range_max) -> float
+	{
+		return range_min + (range_max - range_min) * random_real_number();
 	}
 }
